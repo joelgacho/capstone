69f78ad544bf50c05eda18036e064121c82a08a3
joelgacho <joelgacho78@gmail.com>
2023-03-08 18:56:34 +0800

    fist commit

---
+++ about.php
@@ -0,0 +1,255 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if (isset($_SESSION['user_id'])) {
+   $user_id = $_SESSION['user_id'];
+} else {
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>ShopEase</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+   <?php include 'components/user_header.php'; ?>
+   <!-- header section ends -->
+
+   <div class="heading">
+      <h3>about us</h3>
+      <p><a href="index.php">home</a> <span> / about</span></p>
+   </div>
+
+   <!-- about section starts  -->
+
+   <section class="about">
+
+      <div class="row">
+
+         <div class="image">
+            <img src="images/ban-2.png" alt="about us">
+         </div>
+
+         <div class="content">
+            <h3>why choose us?</h3>
+            <p>Shopease is an online eCommerce website that offers a wide range of products for customers to purchase from the comfort of their homes. </p>
+            <p>Features products from various categories such as electronics, clothing, accessories, and much more. Shopease, customers can enjoy the convenience of online shopping, fast and reliable delivery, and exceptional customer service.</p>
+            <a href="shop.php" class="btn">our product</a>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- about section ends -->
+
+   <!-- steps section starts  -->
+
+   <section class="steps">
+
+      <h1 class="title">simple steps</h1>
+
+      <div class="box-container">
+
+         <div class="box">
+            <img src="images/step-1.png" alt="">
+            <h3>choose order</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, dolorem.</p>
+         </div>
+
+         <div class="box">
+            <img src="images/step-2.png" alt="">
+            <h3>fast delivery</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, dolorem.</p>
+         </div>
+
+         <div class="box">
+            <img src="images/step-3.png" alt="">
+            <h3>item delivered</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, dolorem.</p>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- steps section ends -->
+
+   <!-- reviews section starts  -->
+
+   <section class="reviews">
+
+      <h1 class="title">customer's reivews</h1>
+
+      <div class="swiper reviews-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-1.png" alt="">
+               <p>" I had a fantastic experience shopping with ShopEase! The website was easy to navigate and the checkout process was quick and hassle-free. My order arrived promptly and everything was exactly as described. I will definitely be shopping here again! "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>John Dela T. Cruz</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-2.png" alt="">
+               <p>" I've been a regular customer of ShopEase for a few months now and I'm always impressed with the quality of their products and their exceptional customer service. I had an issue with an order recently and their team went above and beyond to make it right. Highly recommend! "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>Althea Amihan</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-3.png" alt="">
+               <p>" ShopEase has quickly become my go-to online store for all of my shopping needs. They have a great selection of products and their prices are always competitive. Shipping is fast and reliable, and their customer service team is always friendly and helpful. "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>Willis A. Finney</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-4.png" alt="">
+               <p>" I recently discovered ShopEase and I am so glad I did! Their website is user-friendly and their selection of products is fantastic. I ordered some items for my home and they arrived quickly and were well-packaged. I will definitely be a repeat customer! "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>Diwata C. Luwalhati</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-5.png" alt="">
+               <p>" I had a great experience shopping with ShopEase. Their website was easy to navigate and I was able to find exactly what I was looking for. Shipping was fast and my order arrived in perfect condition. I would definitely recommend this store to others! "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>Jeffrey E. Price</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-6.png" alt="">
+               <p>" I've been shopping with ShopEase for a while now and I have nothing but good things to say about them. Their products are high-quality and their customer service is top-notch. They really go above and beyond to make sure their customers are happy. Highly recommend! "</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>Heo Sang-Hee</h3>
+            </div>
+
+         </div>
+
+         <div class="swiper-pagination"></div>
+
+      </div>
+
+   </section>
+
+   <!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts  -->
+   <?php include 'components/footer.php'; ?>
+   <!-- footer section ends -->
+
+
+
+
+
+
+   <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+   <script>
+      var swiper = new Swiper(".reviews-slider", {
+         loop: true,
+         grabCursor: true,
+         spaceBetween: 20,
+         pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+         },
+         breakpoints: {
+            0: {
+               slidesPerView: 1,
+            },
+            700: {
+               slidesPerView: 2,
+            },
+            1024: {
+               slidesPerView: 3,
+            },
+         },
+      });
+   </script>
+
+</body>
+
+</html>
---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">info@shopease.com</a>
+         <a href="mailto:anasbhai@gmail.com">admin@shopease.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 7:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Makati, Manila, Philippines</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">+63 995-892-1190</a>
+         <a href="tel:+63 919-500-3868">+63 919-500-3868</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> <span>KodeGo Capstone</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
